9ae0d8db99d59458fc6dc403e6cd9b6a986cc18f
MohamedYosri-59 <mohamed.yosri986@gmail.com>
2025-02-10 23:33:39 +0200

    Add files via upload

--- ImageCaptionGeneratorByMohamedYosri.py
+++ ImageCaptionGeneratorByMohamedYosri.py
@@ -38,10 +38,25 @@ caption_model_file_id = '1Eix2lcbdrmow_Andf7LyDdNZyKW1YHew'
 download_file_from_google_drive(vgg16_features_file_id, 'vgg16_features.pkl')
 download_file_from_google_drive(caption_model_file_id, 'caption_model.h5')
 
+class NotEqual(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super(CustomLayer, self).__init__(**kwargs)
+
+    def call(self, inputs):
+        x, y = inputs
+        return tf.keras.backend.not_equal(x, y)
+
+# Define the custom layer if it's not already defined
+def NotEqual(x):
+    #return tf.keras.backend.not_equal(x[0], x[1])
+    return tf.keras.backend.not_equal(x, y)
+
+
 # Load the saved model and tokenizer
 @st.cache_resource
 def load_caption_model():
-    return tf.keras.models.load_model("caption_model.h5")
+    with custom_object_scope({'NotEqual': NotEqual}):
+      return tf.keras.models.load_model("caption_model.h5")
 
 @st.cache_data
 def load_tokenizer():

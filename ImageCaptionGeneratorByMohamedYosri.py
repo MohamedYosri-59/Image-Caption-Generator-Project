8968d9f149f221d1af539ecbea7dfa8d15a84faf
MohamedYosri-59 <mohamed.yosri986@gmail.com>
2025-02-10 19:58:49 +0200

    Add files via upload

--- ImageCaptionGeneratorByMohamedYosri.py
+++ ImageCaptionGeneratorByMohamedYosri.py
@@ -20,6 +20,7 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.utils import custom_object_scope
 import gdown  # To download files from Google Drive
 
 
@@ -39,10 +40,16 @@ caption_model_file_id = '1Eix2lcbdrmow_Andf7LyDdNZyKW1YHew'
 download_file_from_google_drive(vgg16_features_file_id, 'vgg16_features.pkl')
 download_file_from_google_drive(caption_model_file_id, 'caption_model.h5')
 
+# Define the custom layer if it's not already defined
+def NotEqual(x):
+    return tf.keras.backend.not_equal(x[0], x[1])
+
 # Load the saved model and tokenizer
 @st.cache_resource
 def load_caption_model():
-    return tf.keras.models.load_model("caption_model.h5")
+    # Register the custom layer when loading the model
+    with custom_object_scope({'NotEqual': tf.keras.backend.not_equal}):
+      return tf.keras.models.load_model("caption_model.h5")
 
 @st.cache_data
 def load_tokenizer():
